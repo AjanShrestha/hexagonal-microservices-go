7fd8b6f007c13e232dfd5a589b68b9e310b77f87
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-23 22:42:21 +1000

    feat: Create the Service logic
    - Implement Find from repo
    - Implement Store in repo

--- shortener/logic.go
+++ shortener/logic.go
@@ -1,9 +1,51 @@
 package shortener
 
-import "errors"
+import (
+	"errors"
+	"time"
+
+	errs "github.com/pkg/errors"
+	"github.com/teris-io/shortid"
+	"gopkg.in/dealancer/validate.v2"
+)
 
 // Custom Errors
 var (
 	ErrRedirectNotFound = errors.New("Redirect Not Found")
 	ErrRedirectInvalid  = errors.New("Redirect Invalid")
 )
+
+// redirectService structure
+// Implements Redirect Repo interface
+type redirectService struct {
+	redirectRepo RedirectRepository
+}
+
+// NewRedirectService instatiates a new redirect service
+func NewRedirectService(redirectRepo RedirectRepository) RedirectService {
+	return &redirectService{
+		redirectRepo,
+	}
+}
+
+// Find returns the Redirect from the code
+func (r *redirectService) Find(code string) (*Redirect, error) {
+	return r.redirectRepo.Find(code)
+}
+
+// Store takes in the redirectService, validate, generates code and stores it
+//
+// - First validate the URL
+//
+// - Generate the code and created at time stamp
+//
+// - Store it and return the error if any
+func (r *redirectService) Store(redirect *Redirect) error {
+	if err := validate.Validate(redirect); err != nil {
+		return errs.Wrap(ErrRedirectInvalid, "service.Redirect.Store")
+	}
+	// 9 digit int64
+	redirect.Code = shortid.MustGenerate()
+	redirect.CreatedAt = time.Now().UTC().Unix()
+	return r.redirectRepo.Store(redirect)
+}
