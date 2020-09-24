b3546bee362316c10881b10a0b7fc850d967ef25
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-25 07:27:51 +1000

    feat: Create mongo client

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
 	github.com/vmihailenco/msgpack/v5 v5.0.0-beta.1
+	go.mongodb.org/mongo-driver v1.4.1
 	gopkg.in/dealancer/validate.v2 v2.1.0
 )
---
+++ repository/mongodb/repository.go
@@ -0,0 +1,35 @@
+package mongo
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+// mongoRepository is the structure for Mongo Client
+// client - Mongo Client
+// database - database name
+// timeout - time it takes for the db instance to timeout
+type mongoRepository struct {
+	client   *mongo.Client
+	database string
+	timeout  time.Duration
+}
+
+// newMongoClient returns the new mongo client
+func newMongoClient(mongoURL string, mongoTimeout int) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(mongoTimeout)*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
+	if err != nil {
+		return nil, err
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+	return client, err
+}
