a7f332743a77a99c497c6f3004d98f6d2d2fa343
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-25 19:57:04 +1000

    feat: Main function to integrate all the services and repo

--- main.go
+++ main.go
@@ -3,18 +3,51 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+
+	h "github.com/AjanShrestha/hexagonal-microservices-go/api"
 	mr "github.com/AjanShrestha/hexagonal-microservices-go/repository/mongodb"
 	rr "github.com/AjanShrestha/hexagonal-microservices-go/repository/redis"
+
 	"github.com/AjanShrestha/hexagonal-microservices-go/shortener"
 )
 
 // repo ⟵ service ⟶ serializer ⟶ http
 
 func main() {
+	repo := choseRepo()
+	service := shortener.NewRedirectService(repo)
+	handler := h.NewHandler(service)
 
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/{code}", handler.Get)
+	r.Post("/", handler.Post)
+
+	errs := make(chan error, 2)
+	go func() {
+		fmt.Printf("Listening on port %s", httpPort())
+		errs <- http.ListenAndServe(httpPort(), r)
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	fmt.Printf("Terminated %s", <-errs)
 }
 
 // httpPort returns the http port to run on from the env
