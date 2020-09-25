bc33e63fc3685cfe411484e81228be04771ca344
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-25 19:49:49 +1000

    feat: Make the repository connection env configurable

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+
+	mr "github.com/AjanShrestha/hexagonal-microservices-go/repository/mongodb"
+	rr "github.com/AjanShrestha/hexagonal-microservices-go/repository/redis"
+	"github.com/AjanShrestha/hexagonal-microservices-go/shortener"
 )
 
 // repo ⟵ service ⟶ serializer ⟶ http
@@ -19,4 +25,28 @@ func httpPort() string {
 		port = os.Getenv("PORT")
 	}
 	return fmt.Sprintf(":%s", port)
+}
+
+// choseRepo returns the database based on the data from env
+func choseRepo() shortener.RedirectRepository {
+	switch os.Getenv("URL_DB") {
+	case "redis":
+		redisURL := os.Getenv("REDIS_URL")
+		repo, err := rr.NewRedisRepository(redisURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	case "mongo":
+		mongoURL := os.Getenv("MONGO_URL")
+		mongodb := os.Getenv("MONGO_DB")
+		mongoTimeout, _ := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
+		repo, err := mr.NewMongoRepository(mongoURL, mongodb, mongoTimeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	default:
+		return nil
+	}
 }
